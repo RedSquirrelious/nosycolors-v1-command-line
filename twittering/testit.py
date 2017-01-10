f4612f7097950462a895507f7a9de992ba6bd0df
RedSquirrelious <redsquirrelious@gmail.com>
2017-01-10 00:04:38 -0800

    testing file

--- twittering/testit.py
+++ twittering/testit.py
@@ -36,3 +36,20 @@ print('Follower Count: ' + str(user.followers_count))
 
 for status in tweepy.Cursor(api.user_timeline).items(2):
 	print(status.text)
+
+target = ''
+
+# thing = api.user_timeline(screen_name=target, count=70, include_rts=False)
+
+thing = api.user_timeline(screen_name=target, count=70)
+
+# print(thing.text)
+
+for tweet in thing:
+
+	# if not tweet.text.startswith('RT'):
+	# 	print(str(tweet.retweet_count))
+		# print(tweet.text)
+	# else:
+	# 	print('you lose')
+# Create your models here.
